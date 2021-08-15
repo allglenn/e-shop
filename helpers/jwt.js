69150efb5c855bf89fdaa4303a416367a123f385
allglenn <glennlenormand@gmail.com>
2021-08-15 12:01:25 +0200

    locked url definition

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,8 +1,16 @@
 const expressJwt = require("express-jwt");
 
-function authJwt() {
+const authJwt = () => {
     const secret = process.env.JWT_SECRET;
-    return expressJwt({secret, algorithms: ["HS256"]})
+    const api = process.env.API_URL;
+    return expressJwt({secret, algorithms: ["HS256"]}).unless({
+        path: [
+            {url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS']},
+            {url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS']},
+            `${api}/users/login`,
+            `${api}/users/register`,
+        ]
+    })
 }
 
 module.exports = authJwt;
