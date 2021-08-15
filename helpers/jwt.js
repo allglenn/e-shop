47a388a0a5d37baf2a7512854b49466f3428ae64
allglenn <glennlenormand@gmail.com>
2021-08-15 18:12:01 +0200

    reject non admin user

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -3,7 +3,11 @@ const expressJwt = require("express-jwt");
 const authJwt = () => {
     const secret = process.env.JWT_SECRET;
     const api = process.env.API_URL;
-    return expressJwt({secret, algorithms: ["HS256"]}).unless({
+    return expressJwt({
+        secret,
+        algorithms: ["HS256"],
+        isRevoked: isRevoked
+    }).unless({
         path: [
             {url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS']},
             {url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS']},
@@ -12,5 +16,10 @@ const authJwt = () => {
         ]
     })
 }
-
+const isRevoked = async (req, payload, done) => {
+    if (!payload.isAdmin) {
+        done(null, true)
+    }
+    done()
+}
 module.exports = authJwt;
