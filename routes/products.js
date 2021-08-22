b22e63a30d3417808c1c1c580642ebe3a30d0000
allglenn <glennlenormand@gmail.com>
2021-08-22 06:16:55 +0200

    add picture to my product

--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,36 @@
 const {Product} = require('../models/product');
 const {Category} = require('../models/category');
 const express = require('express');
+const multer  = require('multer');
+const md5 = require('md5');
 const router = express.Router();
 require('dotenv/config');
+
+const FILE_TYPE_MAP = {
+    'image/png': 'png',
+    'image/jpeg': 'jpeg',
+    'image/jpg': 'jpg'
+}
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const isValid = FILE_TYPE_MAP[file.mimetype];
+        let uploadError = new Error('invalid image type');
+
+        if(isValid) {
+            uploadError = null
+        }
+        cb(uploadError, 'public/uploads')
+    },
+    filename: function (req, file, cb) {
+
+        const fileName = file.originalname.split(' ').join('-');
+        const extension = FILE_TYPE_MAP[file.mimetype];
+        cb(null, `${fileName}-${Date.now()}.${extension}`)
+    }
+})
+
+const uploadOptions = multer({ storage: storage });
 
 router.get(`/`, async (req, res) => {
     let filter ={};
@@ -36,14 +64,16 @@ router.delete(`/:id`, async (req, res) => {
     res.status(204).json({message: "product deleted", success: true});
 })
 
-router.post(`/`, async (req, res) => {
+router.post(`/`, uploadOptions.single("image"),async (req, res) => {
     const category = await Category.findById(req.body.category);
     if (!category) return res.status(400).send('Invalid Category')
+    const fileName = req.file.originalname;
+    const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`;
     let product = new Product({
         name: req.body.name,
         description: req.body.description,
         richDescription: req.body.richDescription,
-        image: req.body.image,
+        image: `${basePath}${fileName}`,
         brand: req.body.brand,
         price: req.body.price,
         category: req.body.category,
